5cf7f0db8e195b6f5cbd15d151e36b2bc8182be6
SebMZI <mrgy.sebastien@gmail.com>
2026-03-05 11:22:21 +0100

    feat: create component pokemons

---
+++ src/app/components/pokemons/pokemons.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemons',
+  imports: [],
+  templateUrl: './pokemons.html',
+  styleUrl: './pokemons.css',
+})
+export class Pokemons {}
