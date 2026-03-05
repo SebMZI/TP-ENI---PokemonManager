fd31dee74aa126acd8849c4bbceacdb9fdec513f
SebMZI <mrgy.sebastien@gmail.com>
2026-03-05 11:29:31 +0100

    feat: integrate generations and pokemons components in app-components.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pokemons } from './components/pokemons/pokemons';
+import { Generations } from './components/generations/generations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Pokemons, Generations],
+  templateUrl: './app-components.html',
+  styleUrl: './app-components.css',
 })
 export class App {
   protected readonly title = signal('PokemonManager');
